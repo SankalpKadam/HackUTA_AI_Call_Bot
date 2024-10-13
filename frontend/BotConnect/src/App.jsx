531da783b645146c17ba72a53cda04c616e57852
Samkit Saraf <63035142+samkit09@users.noreply.github.com>
2024-10-12 20:09:20 -0500

    few working updates

--- frontend/BotConnect/src/App.jsx
+++ frontend/BotConnect/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import '@/index.css'
+
 // Import your components for the routes
 import HomePage from './pages/HomePage.tsx';
 import Dashboard from './pages/Dashboard.tsx';
 import BusinessSetup from './pages/BusinessSetup.tsx';
+import LoginPage from './pages/LoginPage.tsx';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/home" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/buinesssetup" element={<BusinessSetup />} />
         </Routes>
--- frontend/BotConnect/vite.config.js
+++ frontend/BotConnect/vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@':'/src',
+      "@/*": ["./*"],
+      '@src':'/src',
       '@components': '/src/components',
     }
   }
